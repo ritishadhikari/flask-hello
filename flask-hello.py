224b1eab5dbd9ccf8296a6c0fc0fc49f962cf0f8
ritishadhikari <38732657+ritishadhikari@users.noreply.github.com>
2021-04-24 19:20:35 +0530

    Update flask-hello.py

--- flask-hello.py
+++ flask-hello.py
@@ -3,11 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Flask Hello world! Version 4'
+    return 'Flask Hello world! Version 5'
 
 @app.route('/test')
 def test():
-    return 'Testing hidden functionality ;)'
+    return 'Testing hidden functionality, good luck ;)'
 
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0')
